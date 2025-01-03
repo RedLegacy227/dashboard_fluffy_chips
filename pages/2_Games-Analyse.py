b9746e0ddfe55bfa53354aeade2fb4501d1a5098
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-01-03 14:16:30 +0100

    Update 2_Games-Analyse.py

--- pages/2_Games-Analyse.py
+++ pages/2_Games-Analyse.py
@@ -481,14 +481,14 @@ try:
             # Manter apenas as colunas desejadas
             results = results[['Score_Board', 'Probability']]
             
-            # Formatar a coluna Probability com 4 casas decimais
-            results['Probability'] = results['Probability'].apply(lambda x: f"{x:.4f}")
-            
-            # Consolidar valores repetidos na coluna `Score_Board`
+            ## Consolidar valores repetidos na coluna `Score_Board`
             results = results.groupby('Score_Board', as_index=False).agg({'Probability': 'sum'})
             
             # Ordenar pelo valor da probabilidade em ordem decrescente
             results = results.sort_values(by='Probability', ascending=False).reset_index(drop=True)
+            
+            # Formatar a coluna Probability com 4 casas decimais
+            results['Probability'] = results['Probability'].apply(lambda x: f"{x:.4f}")
             
             # Centralizar e estilizar os resultados
             styled_results = results.style.set_properties(**{
