63d762dbf050bf686d45157b37dbf0645e4029b1
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-01-05 22:21:56 +0100

    Update 1_Methods.py

--- pages/1_Methods.py
+++ pages/1_Methods.py
@@ -151,7 +151,7 @@ with tab4:
             data['Odd_Away_Justa'] = (1 / data['P_Away']).round(2)
             
             # Filtro para Back Home
-            back_home_flt = data[(data['Elo_Difference'] > 55) & (data["Perc_Wins_FT_Home"] >= 80)& (data["FT_Odd_H"] <= 3.00) & (data['Media_Saldo_Golos_Home'] > data['Media_Saldo_Golos_Away']) & (data['Media_Ptos_Home']> data['Media_Ptos_Away'])]
+            back_home_flt = data[(data['Elo_Difference'] > 75) & (data["Perc_Wins_FT_Home"] >= 80)& (data["FT_Odd_H"] <= 3.00) & (data['Tilt_Home'] < data['Tilt_Home'])]
             
             # Exibir dados filtrados
             if not back_home_flt.empty:
@@ -179,7 +179,7 @@ with tab5:
                 data['Odd_Away_Justa'] = (1 / data['P_Away']).round(2)
             
             # Filtro para Back Away
-            back_away_flt = data[(data['Elo_Difference'] < -55) & (data["Perc_Wins_FT_Away"] >= 80) & (data["FT_Odd_A"] <= 3.00) & (data['Media_Saldo_Golos_Away'] > data['Media_Saldo_Golos_Home']) & (data['Media_Ptos_Away']> data['Media_Ptos_Home'])]
+            back_away_flt = data[(data['Elo_Difference'] < -75) & (data["Perc_Wins_FT_Away"] >= 80) & (data["FT_Odd_A"] <= 3.00) & (data['Tilt_Away'] < data['Tilt_Home'])]
             
             # Exibir dados filtrados
             if not back_away_flt.empty:
