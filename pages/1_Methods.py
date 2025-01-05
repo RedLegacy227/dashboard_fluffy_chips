f2fa6bb51cb59f71a63beada04dfdbe36d94efd9
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-01-05 22:30:12 +0100

    Update 1_Methods.py

--- pages/1_Methods.py
+++ pages/1_Methods.py
@@ -151,7 +151,7 @@ with tab4:
             data['Odd_Away_Justa'] = (1 / data['P_Away']).round(2)
             
             # Filtro para Back Home
-            back_home_flt = data[(data['Elo_Difference'] > 75) & (data["Perc_Wins_FT_Home"] >= 70)& (data["FT_Odd_H"] <= 3.00) & (data['Media_Golos_Sofridos_Away'] > 1)]
+            back_home_flt = data[(data['Elo_Difference'] > 75) & (data["Perc_Wins_FT_Home"] >= 70)& (data["FT_Odd_H"] <= 3.00) & (data['Media_First_Goal_Home'] < data['Media_First_Goal_Away'])]
             
             # Exibir dados filtrados
             if not back_home_flt.empty:
@@ -179,7 +179,7 @@ with tab5:
                 data['Odd_Away_Justa'] = (1 / data['P_Away']).round(2)
             
             # Filtro para Back Away
-            back_away_flt = data[(data['Elo_Difference'] < -75) & (data["Perc_Wins_FT_Away"] >= 70) & (data["FT_Odd_A"] <= 3.00) & (data['Media_Golos_Sofridos_Home'] > 1  )]
+            back_away_flt = data[(data['Elo_Difference'] < -75) & (data["Perc_Wins_FT_Away"] >= 70) & (data["FT_Odd_A"] <= 3.00) & (data['Media_First_Goal_Away'] < data['Media_First_Goal_Home'])]
             
             # Exibir dados filtrados
             if not back_away_flt.empty:
