3af060a64e1c58fe9c0ac66e690731431c68fb8f
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-02-16 23:28:13 +0100

    Update 1_Methods.py

--- pages/1_Methods.py
+++ pages/1_Methods.py
@@ -68,7 +68,7 @@ if elo_tilt_data is not None:
     st.success("Elo & Tilt Data loaded successfully!")
 
 # Create Tabs
-tabs = ['Lay 0 x 1', 'Lay Any Other Away Win', 'Lay Any Other Home Win', 'Over 1,5 FT', 'Lay Home', 'Lay Away', 'Under 1,5 FT', 'Back Home', 'Lay 1x1']
+tabs = ['Lay 0 x 1', 'Goleada Away', 'Goleada Home', 'Over 1,5 FT', 'Lay Home', 'Lay Away', 'Under 1,5 FT', 'Back Home', 'Lay 1x1']
 tab_views = st.tabs(tabs)
 
 # Exibir dados para cada liga
