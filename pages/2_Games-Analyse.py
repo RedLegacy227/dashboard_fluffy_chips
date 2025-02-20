e1f1582b5488b63afca284f87c312aee77de2877
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-02-20 16:07:25 +0100

    Update 2_Games-Analyse.py

--- pages/2_Games-Analyse.py
+++ pages/2_Games-Analyse.py
@@ -13,7 +13,7 @@ from auth import logout
 from sidebar_menu import show_role_features
 import ast
 
-st.set_page_config(page_title="Games Analyser - Fluffy Chips Web Analyser", page_icon="📽️")
+st.set_page_config(page_title="Games Analyser - Fluffy Chips Web Analyser", page_icon="📽️", layout="wide")
 if "logged_in" not in st.session_state or not st.session_state["logged_in"]:
     st.switch_page("Login.py")  # Redireciona para a página de login
 # ✅ Show role-based features in the sidebar
