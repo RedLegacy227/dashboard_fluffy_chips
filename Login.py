bcb8c7043cc28e86bf74e151341b066980e9f645
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-02-20 03:41:22 +0100

    Update Login.py

--- Login.py
+++ Login.py
@@ -8,7 +8,7 @@ if "logged_in" not in st.session_state:
 # If logged in and redirect flag is set, go to Home_1.py
 if st.session_state.get("redirect_to_1_Home", False):
     st.session_state["redirect_to_1_Home"] = False  # Reset flag
-    st.switch_page("/pages/1_Home.py")
+    st.switch_page("pages/1_Home.py")
 
 # If not logged in, show login page
 if not st.session_state["logged_in"]:
@@ -19,6 +19,6 @@ else:
     # Show "Admin Panel" only for admin users
     if st.session_state.get("role") == "admin":
         if st.sidebar.button("🔑 Admin Panel"):
-            st.switch_page("admin.py")
+            st.switch_page("pages/admin.py")
 
     st.sidebar.button("🚪 Logout", on_click=logout)
