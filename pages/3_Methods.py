063e2b79c3013a9b14dbea759c12bdfa57395054
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-02-20 03:59:39 +0100

    Update 3_Methods.py

--- pages/3_Methods.py
+++ pages/3_Methods.py
@@ -9,7 +9,8 @@ from datetime import datetime
 from auth import logout
 
 # Streamlit App Title and Headers
-st.title('_Fluffy Chips Web Analyzer_')
+st.set_page_config(page_title="Methods - Fluffy Chips", page_icon="🔋")
+st.title("🔋 Methods - Fluffy Chips Web Analyzer")
 st.subheader('The place where you can Analyse Football Matches!!!')
 st.divider()
 st.subheader('_Methods for Today_')
