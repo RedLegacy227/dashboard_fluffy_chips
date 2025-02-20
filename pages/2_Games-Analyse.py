1659dfb7062cca219bbdbf03569a0bb0267df057
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2025-02-20 04:49:51 +0100

    Update 2_Games-Analyse.py

--- pages/2_Games-Analyse.py
+++ pages/2_Games-Analyse.py
@@ -12,8 +12,8 @@ import numpy as np
 from auth import logout
 from ui_helpers import add_logout_button  # ✅ Importa a função para evitar duplicação
 
-#st.set_page_config(page_title="Games Analyser - Fluffy Chips", page_icon="👩🏽‍💻")
-st.title("👩🏽‍💻 Games Analyser - Fluffy Chips")
+st.set_page_config(page_title="Games Analyser - Fluffy Chips", page_icon="📽️")
+st.title("📽️ Games Analyser - Fluffy Chips")
 st.subheader('The place where you can Analyse Football Matches!!!')
 st.divider()
 st.subheader('_Games of the Day_')
